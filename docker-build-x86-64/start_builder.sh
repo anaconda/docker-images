ac6f3ff17173359f8b2c9cb7827f1372b9817899
Michael Sarahan <msarahan@gmail.com>
2016-02-15 12:14:16 -0600

    fix initial output when entering builder image

--- docker-build-x86-64/start_builder.sh
+++ docker-build-x86-64/start_builder.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 user=$(id -u -n)
 home=$(eval echo "~${user}")
-id=$(docker run -ti -d -e USER=$user \
-       -v $home/.ssh:/home/dev/.ssh:ro \
+
+ssh_vol=""
+if [ -d "$home/.ssh" ]; then
+   ssh_vol="-v $home/.ssh:/home/dev/.ssh:ro"
+fi
+
+gitconfig_vol=""
+if [ -e $home/.gitconfig ]; then
+    gitconfig_vol="-v $home/.gitconfig:/home/dev/.gitconfig:ro"
+fi
+
+docker run -ti -e USER=$user \
+       $ssh_vol \
+       $gitconfig_vol \
        msarahan/conda_builder_linux:latest \
-       /bin/bash)
-docker cp $home/.gitconfig $id:/home/dev/.gitconfig
-docker attach $id
+       bash -c 'echo "Welcome to the conda-builder image.  \
+clone_recipes will clone the conda-recipes repo.";bash'
