7641713eb9daca8874d587e6cd3b2493d4c3f581
Michael Sarahan <msarahan@gmail.com>
2016-03-28 11:02:25 -0400

    filter uname output to trick build scripts into using the multilib compiler

--- conda_builder_linux/build/internal_startup.sh
+++ conda_builder_linux/build/internal_startup.sh
@@ -5,6 +5,11 @@ if [[ -z "${ABI}" ]]; then
     export ABI=4
 fi
 
+# replace ancient system libstdc++, plug ours in.  This is a workaround for some recipes that do not respect LIBRARY_PATH.
+sudo ln -sf /usr/local/lib64/libstdc++.so.6.0.21 /usr/lib64/libstdc++.so.6
+sudo ln -sf /usr/local/lib/libstdc++.so.6.0.21 /usr/lib/libstdc++.so.6
+sudo ln -sf /usr/local/lib/libgcc_s.so.1 /usr/lib/libgcc_s.so.1
+
 # Setup home environment
 
 export PATH=/usr/local/bin:/opt/miniconda/bin:$PATH
@@ -24,6 +29,21 @@ echo "alias clone_recipes='git clone https://github.com/conda/conda-recipes'" >>
 echo "alias clone_anaconda='git clone git@github.com:continuumIO/anaconda'" >> ~/.bashrc
 echo "alias anaconda_setup='clone_anaconda && cd anaconda && git checkout use_hbb && python setup.py develop && cd .. && mkdir aroot'" >> ~/.bashrc
 
+if [[ "$ARCH" -eq "64" || -z "$ARCH" ]]; then
+    ARCH_DOC=$'To build 32-bit code, set the ARCH environment
+        variable to 32. (-e \"ARCH=32\" docker argument)'
+else
+    ARCH_DOC=$'Pretending to be i686 (using multilib gcc) and
+        filtering uname to output i686.'
+    export CFLAGS="${CFLAGS} -m32"
+    export CXXFLAGS="${CXXFLAGS} -m32"
+    export CONDA_FORCE_32BIT=1
+    # this will get picked up by subshells
+    sudo mv /bin/uname /bin/uname_x64
+    sudo ln -s /opt/share/alias_32bit.sh /bin/uname
+    sudo chmod +x /bin/uname
+fi
+
 echo
 echo "Welcome to the conda-builder image, brought to you by Continuum Analytics."
 echo
@@ -31,19 +51,19 @@ echo "Binaries produced with this image should be compatible with any Linux OS"
 echo "that is at least CentOS 5 or newer (Glibc lower bound), and anything "
 echo "that uses G++ 5.2 or older (libstdc++ upper bound)"
 echo
-echo "   GCC is: $(gcc --version | head -1)"
-echo "   Default C++ ABI: ${ABI} (C++$([ "${ABI}" == "4" ] && echo "98" || echo "11"))"
-echo "   GLIBC is: $(getconf GNU_LIBC_VERSION)"
-echo "   Native arch is x64.  To build for 32-bit, set CONDA_FORCE_32BIT=1, or"
-echo "        outside of conda-build, CFLAGS=\"-m32\""
-echo "   ld/binutils is: $(ld --version | head -1)"
+echo "    GCC is: $(gcc --version | head -1)"
+echo "    Default C++ ABI: ${ABI} (C++$([ "${ABI}" == "4" ] && echo "98" || echo "11"))"
+echo "    GLIBC is: $(getconf GNU_LIBC_VERSION)"
+echo "    ld/binutils is: $(ld --version | head -1)"
 echo
-echo "   The dev user (currently signed in) has passwordless sudo access."
-echo "   miniconda (2.7) is installed at /opt/miniconda."
-echo "   git is also available."
+echo "    Native arch is x86_64.  ${ARCH_DOC}"
+echo
+echo "    The dev user (currently signed in) has passwordless sudo access."
+echo "    miniconda (2.7) is installed at /opt/miniconda."
+echo "    git is also available."
 
 if [ -f "/home/dev/.gitconfig" ]; then
-    echo "   Your .gitconfig has been imported."
+    echo "    Your .gitconfig has been imported."
 fi
 
 # jumping through hoops for file ownership - we can't have the owner be
@@ -57,7 +77,7 @@ if [ -f /id_rsa ]; then
     sudo chmod 600 .ssh/id_rsa
     echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
     echo -e "Host bremen\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-    echo "   Your ssh private key has been imported for passwordless ssh."
+    echo "    Your ssh private key has been imported for passwordless ssh."
 fi
 
 echo
