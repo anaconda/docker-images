6da3641f917c86d56b5feb08794e7c34470b67ce
Tyler Kim <tkim139@illinois.edu>
2017-03-16 13:35:40 -0500

    added centos-gcc44

---
+++ centos5_gcc44/README.md
@@ -0,0 +1,9 @@
+# centos5_gcc44_base
+
+Docker image (CentOS 5.11) with GCC 44, Binutils 2.17.
+
+Libraries built with this docker container should be compatible with most Linux
+distributions.
+
+This image is limited. It is meant as a base layer for other tools
+that utilize the compiler.
---
+++ centos5_gcc44/install_yum_deps.sh
@@ -0,0 +1,12 @@
+yum install -y curl.x86_64 bzip2.x86_64 yum-utils glibc-devel patch \
+    unzip bison yasm file make libtool.x86_64 pkgconfig.x86_64 \
+    ison byacc cscope ctags cvs diffstat doxygen flex gettext \
+    indent intltool libtool curl bzip2 wget swig systemtap\
+    patch patchutils rcs redhat-rpm-config rpm-build subversion \
+    centos-release-SCL devtoolset-2
+    emac vim-enhanced nano \
+
+yum install -y  gcc44 gcc44-c++ gcc44-gfortran mesa-libGL-devel \
+                 mesa-libGLU-devel libX11-devel libXt-devel \
+                 libXrender-devel libXext-devel libXdmcp-devel \
+                 java-1.7.0-openjdk java-1.7.0-openjdk-devel libgcj
