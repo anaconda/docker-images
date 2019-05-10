f2a96ba65f386a633cfafb7f55bacb00e66dfb5f
Rich <rich@tongfamily.com>
2019-05-09 20:48:08 -0400

    build_anaconda abstracts out continuumio as the repo name

--- scripts/build_anaconda.sh
+++ scripts/build_anaconda.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 function usage {
-    echo -e "Usage: ./build_anaconda.sh -v version \n"
+    echo -e "Usage: ./build_anaconda.sh -v version [-r repository] \n"
     exit 1
 }
+REPO="continuumio"
 
 while [[ $# -gt 0 ]]
 do
 key="$1"
 
 case $key in
-    -v|--version)
+  -v|--version)
     VERSION="$2"
     shift
     shift
     ;;
-    *)
+  -r|--repository)
+    REPO="$2"
+    shift
+    shift
+    ;;
+  *)
     usage
     exit 0
     shift
@@ -28,23 +34,23 @@ cd ..
 echo "ANACONDA ALPINE"
 
 pushd anaconda/alpine
-docker build . -t continuumio/anaconda:$VERSION-alpine
+docker build . -t "$REPO/anaconda:$VERSION-alpine"
 popd
 
 echo "ANACONDA DEBIAN"
 
 pushd anaconda/debian
-docker build . -t continuumio/anaconda:$VERSION -t continuumio/anaconda:latest
+docker build . -t $REPO/anaconda:$VERSION -t $REPO/anaconda:latest
 popd
 
 echo "ANACONDA3 ALPINE"
 
 pushd anaconda3/alpine
-docker build . -t continuumio/anaconda3:$VERSION-alpine
+docker build . -t $REPO/anaconda3:$VERSION-alpine
 popd
 
 echo "ANACONDA3 DEBIAN"
 
 pushd anaconda3/debian
-docker build . -t continuumio/anaconda3:$VERSION -t continuumio/anaconda3:latest
+docker build . -t $REPO/anaconda3:$VERSION -t $REPO/anaconda3:latest
 popd
