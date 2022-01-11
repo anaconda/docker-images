f8a23e3303f037f6b39b2270b488c3fc0e48274c
Daniel Komisar <dkomisar@anaconda.com>
2022-01-10 21:21:41 -0500

    argument for workdir

--- anaconda-pkg-build/windows/run-build.sh
+++ anaconda-pkg-build/windows/run-build.sh
@@ -3,12 +3,19 @@
 # needed for dirname for activate below
 PATH=$PATH:/usr/bin
 
+# get working directory
+WORKDIR="$1"
+shift 1
+
 # save commands and reset them otherwise activate will assume arguments are for it
 COMMAND=("$@")
 set --
 
 # shellcheck source=/dev/null
 source /c/Users/Administrator/miniconda3/Scripts/activate
+
+# change to working directory
+cd "$WORKDIR"
 
 # execute command
 ${COMMAND[@]}
