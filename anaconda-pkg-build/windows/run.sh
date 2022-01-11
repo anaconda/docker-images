729cc79ea429ef5395c0ee438b1157bcd4a1a53b
Daniel Komisar <dkomisar@anaconda.com>
2022-01-11 14:49:12 -0500

    get rid of workdir option

--- anaconda-pkg-build/windows/run.sh
+++ anaconda-pkg-build/windows/run.sh
@@ -2,14 +2,12 @@
 
 set -o nounset -o pipefail -o errexit
 
+#COMMAND=("$@")
+#${COMMAND[@]}
+#exit
+
 # needed for dirname for activate below
 PATH=$PATH:/usr/bin
-
-# change working directory
-if [[ "$1" -eq -w ]]; then
-    cd "$2"
-    shift 2
-fi
 
 # save commands and reset them otherwise activate will assume arguments are for it
 COMMAND=("$@")
