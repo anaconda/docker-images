5529a52fe741d5abd1f310f78f8f26c1c7e88092
Daniel Komisar <dkomisar@anaconda.com>
2022-01-11 16:13:19 -0500

    comment out missing build tools

--- anaconda-pkg-build/windows/entry-bash.bat
+++ anaconda-pkg-build/windows/entry-bash.bat
@@ -2,7 +2,7 @@
 @REM This exists to work around unexpected behavior in Windows docker entrypoints
 
 C:\Users\Administrator\miniconda3\Scripts\activate.bat
-C:\BuildTools\VC\Auxiliary\Build\vcvars64.bat -vcvars_ver=14.16 8.1
+@REM C:\BuildTools\VC\Auxiliary\Build\vcvars64.bat -vcvars_ver=14.16 8.1
 
 @REM runner.sh is necessary b/c if -c was used then we'd have to quote the args as a single string
 @REM and they probably wouldn't parse back into the same arguments because the quotes aren't escaped
--- anaconda-pkg-build/windows/entry-cmd.bat
+++ anaconda-pkg-build/windows/entry-cmd.bat
@@ -1,11 +1,7 @@
 @REM Setups up conda and compiler environments and then executes args
 @REM This exists to work around unexpected behavior in Windows docker entrypoints
 
-@ECHO ON
-echo Running entry-cmd.bat
-
 C:\Users\Administrator\miniconda3\Scripts\activate.bat
-C:\BuildTools\VC\Auxiliary\Build\vcvars64.bat -vcvars_ver=14.16 8.1
+@REM C:\BuildTools\VC\Auxiliary\Build\vcvars64.bat -vcvars_ver=14.16 8.1
 
-@ECHO ON
 %*
