59a431bd55222bc7356033cff8c7cd2bc88c4ccf
Daniel Komisar <dkomisar@anaconda.com>
2022-01-11 15:26:26 -0500

    add echo on

--- anaconda-pkg-build/windows/entry-bash.bat
+++ anaconda-pkg-build/windows/entry-bash.bat
@@ -1,12 +1,10 @@
-@REM Passes all arguments to run.sh
+@REM Passes all arguments to runner.sh after setting up conda and compiler environments
 @REM This exists to work around unexpected behavior in Windows docker entrypoints
-@REM Windows has problems using anything except a single command (i.e. no args) in entrypoints
-@REM Without this you'd only be able to us plain bash as the entry point, and then
-@REM you'd always have to remember to use run.sh.
 
 C:\Users\Administrator\miniconda3\Scripts\activate.bat
 C:\BuildTools\VC\Auxiliary\Build\vcvars64.bat -vcvars_ver=14.16 8.1
 
 @REM runner.sh is necessary b/c if -c was used then we'd have to quote the args as a single string
 @REM and they probably wouldn't parse back into the same arguments because the quotes aren't escaped
+@ECHO ON
 C:\Users\Administrator\miniconda3\Library\usr\bin\bash.exe /c/Administrator/Users/runner.sh %*
