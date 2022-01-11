9bc243eec9d39f6432ebcaef0aa5ad57b642e7bd
Daniel Komisar <dkomisar@anaconda.com>
2022-01-11 15:48:09 -0500

    debug

--- anaconda-pkg-build/windows/entry-cmd.bat
+++ anaconda-pkg-build/windows/entry-cmd.bat
@@ -1,6 +1,9 @@
 @REM Setups up conda and compiler environments and then executes args
 @REM This exists to work around unexpected behavior in Windows docker entrypoints
 
+@ECHO ON
+echo Running entry-cmd.bat
+
 C:\Users\Administrator\miniconda3\Scripts\activate.bat
 C:\BuildTools\VC\Auxiliary\Build\vcvars64.bat -vcvars_ver=14.16 8.1
 
